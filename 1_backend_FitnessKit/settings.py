4f65d7dd094befb6426d15e461379b8d80388cf8
arsenchik1 <arsen1234507@gmail.com>
2026-02-20 21:24:02 +0300

    Delete 1_backend_FitnessKit directory

--- 1_backend_FitnessKit/settings.py
+++
@@ -1,5 +0,0 @@
-# настройки подключения к внешней системе
-EXTERNAL_API_URL = os.getenv('EXTERNAL_API_URL', 'http://176.192.70.122:90/fitnes_t_nfc_mobile/hs/nfc_mobile/v1')
-EXTERNAL_LOGIN = os.getenv('EXTERNAL_LOGIN', 'FitnessKit')
-EXTERNAL_PASSWORD = os.getenv('EXTERNAL_PASSWORD', 'vY0xodyg')
-EXTERNAL_CLUB_ID = os.getenv('EXTERNAL_CLUB_ID', '59115d1e-9052-11eb-810c-6eae8b56243b')
