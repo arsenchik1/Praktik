c53b273f6f067d54c1f23adfb47f42451a5a5ee3
arsenchik1 <arsen1234507@gmail.com>
2026-02-25 16:25:26 +0300

    Delete config directory

--- config/asgi_api.py
+++
@@ -1,9 +0,0 @@
-"""
-ASGI config for API only (без сессий и статики)
-"""
-import os
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings_api')
-
-application = get_asgi_application()
